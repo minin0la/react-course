0ba285a4c182a523ece446f752307df05e05a946
Natawat Hebnak <minin0la17@gmail.com>
2020-12-10 00:26:29 +0800

    useEffect Fetch Data

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/3-useState-array";
-import Final from "./tutorial/1-useState/final/1-error-example";
+import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 function App() {
   return (
     <div className="container">
       <Setup />
-      {/* <Final /> */}
     </div>
   );
 }
